0ab96680ab4368aaeaa24571b69950e1a253a3a7
Customer Engineering <admin@srv.demo.crate.io>
2021-11-12 16:14:09 +0100

    FIX: Added daily export of data from day before the current day

--- dags/table-export-dag.py
+++ dags/table-export-dag.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
@@ -6,13 +7,13 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 with DAG(
     dag_id="cratedb_table_export",
     start_date=datetime.datetime(2021, 11, 11),
-    schedule_interval="@hourly",
+    schedule_interval="@daily",  # start at 00:00
     catchup=False,
 ) as dag:
     PostgresOperator(
         task_id="copy_to",
         postgres_conn_id="cratedb_connection",
         sql="""
-           COPY telegraf.metrics TO DIRECTORY 's3://ACCESS_KEY:SECRET_KEY@crate-astro-tutorial/data';
-          """,
+                COPY telegraf.metrics WHERE DATE_TRUNC('day', timestamp) = {time}::TIMESTAMP - '1 day'::INTERVAL TO DIRECTORY 's3://{access}:{secret}@crate-astro-tutorial/data'; 
+            """.format(time='{{ ds }}', access=os.environ.get('ACCESS_KEY'), secret=os.environ.get('SECRET_KEY')),
     )
