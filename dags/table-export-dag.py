fa10b875b66f5899d24b92e9be56d494a95fd31f
Customer Engineering <admin@srv.demo.crate.io>
2021-11-15 10:21:19 +0100

    FIX: Added date extended directory name for S3

--- dags/table-export-dag.py
+++ dags/table-export-dag.py
@@ -7,13 +7,18 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 with DAG(
     dag_id="cratedb_table_export",
     start_date=datetime.datetime(2021, 11, 11),
-    schedule_interval="@daily", # start at 00:00
+    schedule_interval="@daily",  # start at 00:00
     catchup=False,
 ) as dag:
     PostgresOperator(
         task_id="copy_to",
         postgres_conn_id="cratedb_connection",
         sql="""
-                COPY telegraf.metrics WHERE DATE_TRUNC('day', timestamp) = {time}::TIMESTAMP - '1 day'::INTERVAL TO DIRECTORY 's3://{access}:{secret}@crate-astro-tutorial/data'; 
-            """.format(time='{{ ds }}', access=os.environ.get('ACCESS_KEY'), secret=os.environ.get('SECRET_KEY')),
+                COPY telegraf.metrics WHERE DATE_TRUNC('day', timestamp) = {time}::TIMESTAMP - '1 day'::INTERVAL 
+                TO DIRECTORY 's3://{access}:{secret}@crate-astro-tutorial/data_{time}'; 
+            """.format(
+            time="{{ ds }}",
+            access=os.environ.get("ACCESS_KEY"),
+            secret=os.environ.get("SECRET_KEY"),
+        ),
     )
