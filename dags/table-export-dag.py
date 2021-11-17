c63b4b89841ac2da76340dcc5dfaaeba23aebabb
Niklas Schmidtmer <niklas@crate.io>
2021-11-17 13:42:50 +0100

    * Align environment variable names with the community post * Remove trailing spaces

--- dags/table-export-dag.py
+++ dags/table-export-dag.py
@@ -7,18 +7,18 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 with DAG(
     dag_id="cratedb_table_export",
     start_date=datetime.datetime(2021, 11, 11),
-    schedule_interval="@daily",  # start at 00:00
+    schedule_interval="@daily",
     catchup=False,
 ) as dag:
     PostgresOperator(
         task_id="copy_to",
         postgres_conn_id="cratedb_connection",
         sql="""
-                COPY telegraf.metrics WHERE DATE_TRUNC('day', timestamp) = {time}::TIMESTAMP - '1 day'::INTERVAL 
-                TO DIRECTORY 's3://{access}:{secret}@crate-astro-tutorial/data_{time}'; 
+                COPY telegraf.metrics WHERE DATE_TRUNC('day', timestamp) = {time}::TIMESTAMP - '1 day'::INTERVAL
+                TO DIRECTORY 's3://{access}:{secret}@crate-astro-tutorial/data_{time}';
             """.format(
             time="{{ ds }}",
-            access=os.environ.get("ACCESS_KEY"),
-            secret=os.environ.get("SECRET_KEY"),
+            access=os.environ.get("ACCESS_KEY_ID"),
+            secret=os.environ.get("SECRET_ACCESS_KEY"),
         ),
     )
