ff884ab63f708558684e0a7f8e20549a9297123d
Customer Engineering <admin@srv.demo.crate.io>
2021-11-18 17:59:13 +0100

    added query parameterization

--- dags/data-cleanup-dag.py
+++ dags/data-cleanup-dag.py
@@ -11,27 +11,29 @@ from airflow.models import Variable
 
 
 # define a function that connects to cratedb and fetches policies
-def get_records(sql):
+def get_policies(sql):
     pg_hook = PostgresHook(postgres_conn_id="cratedb_connection")
     records = pg_hook.get_records(sql=sql)
     json_output = json.dumps(records)
     return json_output
 
 
-def analyze_output(ti):
-    policies = ti.xcom_pull(task_ids="push_results")
+def delete_partitions(ti):
+    policies = ti.xcom_pull(task_ids="retrieve_retention_policies")
     json_object = json.loads(policies)
     for p in json_object:
-        logging.info(p[2])
-        key = list(p[2].keys())[0]
-        value = p[2][key]
-        logging.info(type(value))
+        key = list(p[1].keys())[0]
+        value = p[1][key]
+        logging.info(p[1])
         PostgresOperator(
             task_id="delete_from_{table}".format(table=str(p[1])),
             postgres_conn_id="cratedb_connection",
-            sql="DELETE FROM {schema}.{table} WHERE {column}={value}".format(
-                schema=str(p[0]), table=str(p[1]), column=str(key), value=value
-            ),
+            sql="DELETE FROM %(table)s WHERE %(column)s=%(value)s",
+            parameters={
+                "table": str(p[0]),
+                "column": str(key),
+                "value": value,
+            },
         ).execute(dict())
 
 
@@ -43,22 +45,20 @@ with DAG(
 ) as dag:
     t1 = (
         PythonOperator(
-            task_id="push_results",
-            python_callable=get_records,
+            task_id="retrieve_retention_policies",
+            python_callable=get_policies,
             op_kwargs={
-                "sql": " SELECT p.table_schema, p.table_name, p.values FROM information_schema.table_partitions p JOIN doc.retention_policies r ON p.table_schema = r.table_schema AND p.table_name = r.table_name AND p.values[r.partition_column] < CURDATE() - r.retention_period;"
+                "sql": " SELECT QUOTE_IDENT(p.table_schema) || '.' || QUOTE_IDENT(p.table_name) as fqn, p.values FROM information_schema.table_partitions p JOIN doc.retention_policies r ON p.table_schema = r.table_schema AND p.table_name = r.table_name AND p.values[r.partition_column] < CURDATE() - r.retention_period;"
             },
             dag=dag,
         ),
     )
-
     t2 = PythonOperator(
-        task_id="delete_records",
-        python_callable=analyze_output,
+        task_id="apply_data_retention_policies",
+        python_callable=delete_partitions,
         provide_context=True,
         op_kwargs={},
         dag=dag,
     )
-
 
 t1 >> t2
