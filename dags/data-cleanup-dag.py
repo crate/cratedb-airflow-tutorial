23b0aadc22c90c794238e6659a79f40d3f8f3eee
Customer Engineering <admin@srv.demo.crate.io>
2021-11-19 13:33:21 +0100

    pull request fixed

--- dags/data-cleanup-dag.py
+++ dags/data-cleanup-dag.py
@@ -1,6 +1,5 @@
 import datetime
 import json
-import logging
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.hooks.postgres_hook import PostgresHook
@@ -17,43 +16,45 @@ def get_policies(sql):
 def delete_partitions(ti):
     retention_policies = ti.xcom_pull(task_ids="retrieve_retention_policies")
     policies_obj = json.loads(retention_policies)
-    for p in policies_obj:
-        key = list(p[1].keys())[0]
-        value = p[1][key]
+    for policy in policies_obj:
+        table_name = policy[0]
+        column_name = policy[1]
+        partition_value = policy[2]
         PostgresOperator(
-            task_id="delete_from_{table}".format(table=str(p[1])),
+            task_id="delete_from_{table}".format(table=str(table_name)),
             postgres_conn_id="cratedb_connection",
             sql="DELETE FROM %(table)s WHERE %(column)s=%(value)s",
             parameters={
-                "table": str(p[0]),
-                "column": str(key),
-                "value": value,
+                "table": str(table_name),
+                "column": str(column_name),
+                "value": partition_value,
             },
         ).execute(dict())
 
 
 with DAG(
     dag_id="data-cleanup-dag",
-    start_date=datetime.datetime(2021, 11, 17),
+    start_date=datetime.datetime(2021, 11, 19),
     schedule_interval="@daily",
     catchup=False,
 ) as dag:
-    t1 = (
-        PythonOperator(
-            task_id="retrieve_retention_policies",
-            python_callable=get_policies,
-            op_kwargs={
-                "sql": """ SELECT QUOTE_IDENT(p.table_schema) || '.' || QUOTE_IDENT(p.table_name) as fqn, p.values 
-                           FROM information_schema.table_partitions p JOIN doc.retention_policies r ON p.table_schema = r.table_schema 
-                           AND p.table_name = r.table_name AND p.values[r.partition_column] < CURDATE() - r.retention_period;"""
-            },
-        ),
+    get_policies = PythonOperator(
+        task_id="retrieve_retention_policies",
+        python_callable=get_policies,
+        op_kwargs={
+            "sql": """ SELECT QUOTE_IDENT(p.table_schema) || '.' || QUOTE_IDENT(p.table_name) as fqn, 
+                        r.partition_column, p.values[r.partition_column] 
+                       FROM information_schema.table_partitions p JOIN doc.retention_policies r ON p.table_schema = r.table_schema 
+                       AND p.table_name = r.table_name 
+                       AND p.values[r.partition_column] < {date}::TIMESTAMP - r.retention_period;"""
+            .format(date="{{ ds }}")
+        },
     )
-    t2 = PythonOperator(
+    apply_policies = PythonOperator(
         task_id="apply_data_retention_policies",
         python_callable=delete_partitions,
         provide_context=True,
         op_kwargs={},
     )
 
-t1 >> t2
+    get_policies >> apply_policies
