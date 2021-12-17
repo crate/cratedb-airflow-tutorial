e45d60501d10e4fb0be6b4ddb968be68ff62c514
Niklas Schmidtmer <niklas@crate.io>
2021-12-17 09:59:33 +0100

    Always require airflow

--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ setup(
     name="crate-airflow-tutorial",
     packages=find_packages(),
     python_requires=">=3.8",
+    install_requires=[
+        "apache-airflow==2.2.2",
+        "apache-airflow-providers-postgres==2.4.0",
+    ],
     extras_require={
         "develop": [
             "pylint==2.12.2",
         ],
         "testing": [
-            "apache-airflow==2.2.2",
             "pytest==6.2.5",
-            "apache-airflow-providers-postgres==2.4.0",
         ],
     },
 )
