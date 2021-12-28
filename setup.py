f660a22e8e9a30796932b54629cada3c02a34fcb
Niklas Schmidtmer <niklas@crate.io>
2021-12-28 10:47:06 +0100

    Align Airflow version with updated Astronomer Docker stack

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     python_requires=">=3.8",
     install_requires=[
-        "apache-airflow==2.2.2",
+        "apache-airflow==2.2.3",
         "apache-airflow-providers-postgres==2.4.0",
     ],
     extras_require={
