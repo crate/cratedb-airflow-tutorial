64bfa630d0771d638390437e419c94c78c58a26e
Niklas Schmidtmer <niklas@crate.io>
2021-12-28 08:37:43 +0100

    Remove path insertion after moving config file to include

--- tests/dag_import_test.py
+++ tests/dag_import_test.py
@@ -1,9 +1,6 @@
 import sys
 from airflow.models import DagBag
 
-# ensure dags/config gets loaded
-sys.path.insert(0, 'dags')
-
 # Verifies that all DAGs can be loaded successfully
 def test_no_import_errors():
     dag_bag = DagBag(dag_folder='dags', include_examples=False)
