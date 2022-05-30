cab79e2418aa3d72c830ee260315fcb4e317dd78
Niklas Schmidtmer <niklas@crate.io>
2022-05-30 14:18:46 +0200

    Adapt data rention DAGs to use dynamic task mapping

--- dags/data_retention_reallocate_dag.py
+++ dags/data_retention_reallocate_dag.py
@@ -1,106 +1,67 @@
 """
 Implements a retention policy by reallocating cold partitions
 
+A detailed tutorial is available at https://community.crate.io/t/cratedb-and-apache-airflow-building-a-hot-cold-storage-data-retention-policy/934
+
 Prerequisites
 -------------
 In CrateDB, tables for storing retention policies need to be created once manually.
 See the file setup/data_retention_schema.sql in this repository.
 """
-import json
-import logging
 from pathlib import Path
 import pendulum
-from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
 
-
-def get_policies(logical_date):
+# Retrieve all partitions effected by a policy
+@task
+def get_policies(ds=None):
     pg_hook = PostgresHook(postgres_conn_id="cratedb_connection")
     sql = Path('include/data_retention_retrieve_reallocate_policies.sql') \
-              .read_text(encoding="utf-8").format(date=logical_date)
-    records = pg_hook.get_records(sql=sql)
+            .read_text(encoding="utf-8").format(date=ds)
+    return pg_hook.get_records(sql=sql)
 
-    return json.dumps(records)
-
-
-def reallocate_partition(partition):
-    logging.info("Reallocating partition %s = %s for table %s to %s = %s",
-                 partition["column_name"],
-                 partition["partition_value"],
-                 partition["table_fqn"],
-                 partition["reallocation_attribute_name"],
-                 partition["reallocation_attribute_value"],
+# Generate SQL for reallocation
+@task
+def generate_sql_reallocate(policy):
+    return Path('include/data_retention_reallocate.sql').read_text(encoding="utf-8").format(
+        table=policy[2],
+        column=policy[3],
+        value=policy[4],
+        attribute_name=policy[5],
+        attribute_value=policy[6],
     )
 
-    # Reallocate the partition
-    PostgresOperator(
-        task_id=f"reallocate_{partition['table_fqn']}" \
-                f"_{partition['column_name']}_{partition['partition_value']}",
-        postgres_conn_id="cratedb_connection",
-        sql=Path('include/data_retention_reallocate.sql').read_text(encoding="utf-8").format(
-            table=partition["table_fqn"],
-            column=partition["column_name"],
-            value=partition["partition_value"],
-            attribute_name=partition["reallocation_attribute_name"],
-            attribute_value=partition["reallocation_attribute_value"],
-        ),
-    ).execute({})
+# Generate SQL for tracking entry
+@task
+def generate_sql_tracking(policy):
+    return Path('include/data_retention_reallocate_tracking.sql').read_text(encoding="utf-8") \
+            .format(schema=policy[0],
+                    table=policy[1],
+                    partition_value=policy[4],
+                   )
 
-    # Update tracking information. As we process partitions in ascending order
-    # by the partition value, it is safe to always save the current value.
-    PostgresOperator(
-        task_id=f"reallocate_track_{partition['table_fqn']}" \
-                f"_{partition['column_name']}_{partition['partition_value']}",
-        postgres_conn_id="cratedb_connection",
-        sql=Path('include/data_retention_reallocate_tracking.sql') \
-                .read_text(encoding="utf-8") \
-                .format(table=partition["table_name"],
-                        schema=partition["schema_name"],
-                        partition_value=partition["partition_value"],
-                        ),
-    ).execute({})
-
-
-def map_policy(policy):
-    return {
-        "schema_name": policy[0],
-        "table_name": policy[1],
-        "table_fqn": policy[2],
-        "column_name": policy[3],
-        "partition_value": policy[4],
-        "reallocation_attribute_name": policy[5],
-        "reallocation_attribute_value": policy[6],
-    }
-
-
-def reallocate_partitions(ti):
-    retention_policies = ti.xcom_pull(task_ids="retrieve_retention_policies")
-    policies_obj = json.loads(retention_policies)
-
-    for policy in policies_obj:
-        reallocate_partition(map_policy(policy))
-
-
-with DAG(
-    dag_id="data-retention-reallocate-dag",
+@dag(
     start_date=pendulum.datetime(2021, 11, 19, tz="UTC"),
     schedule_interval="@daily",
     catchup=False,
-) as dag:
-    get_policies = PythonOperator(
-        task_id="retrieve_retention_policies",
-        python_callable=get_policies,
-        op_kwargs={
-            "logical_date": "{{ ds }}",
-        },
-    )
+)
+def data_retention_reallocate():
+    policies = get_policies()
+    sql_statements_reallocate = generate_sql_reallocate.expand(policy=policies)
+    sql_statements_tracking = generate_sql_tracking.expand(policy=policies)
 
-    apply_policies = PythonOperator(
-        task_id="apply_data_retention_policies",
-        python_callable=reallocate_partitions,
-        op_kwargs={},
-    )
+    reallocate = PostgresOperator.partial(
+        task_id="reallocate_partitions",
+        postgres_conn_id="cratedb_connection",
+    ).expand(sql=sql_statements_reallocate)
 
-    get_policies >> apply_policies
+    track = PostgresOperator.partial(
+        task_id="add_tracking_information",
+        postgres_conn_id="cratedb_connection",
+    ).expand(sql=sql_statements_tracking)
+
+    reallocate >> track
+
+data_retention_reallocate_dag = data_retention_reallocate()
--- dags/data_retention_snapshot_dag.py
+++ dags/data_retention_snapshot_dag.py
@@ -1,98 +1,68 @@
 """
 Implements a retention policy by snapshotting expired partitions to a repository
 
+A detailed tutorial is available at https://community.crate.io/t/cratedb-and-apache-airflow-building-a-data-retention-policy-using-external-snapshot-repositories/1001
+
 Prerequisites
 -------------
 In CrateDB, tables for storing retention policies need to be created once manually.
 See the file setup/data_retention_schema.sql in this repository.
 """
-import json
-import logging
 from pathlib import Path
 import pendulum
-from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
 
-
-def get_policies(logical_date):
+# Retrieve all partitions effected by a policy
+@task
+def get_policies(ds=None):
     pg_hook = PostgresHook(postgres_conn_id="cratedb_connection")
     sql = Path('include/data_retention_retrieve_snapshot_policies.sql') \
-              .read_text(encoding="utf-8").format(date=logical_date)
-    records = pg_hook.get_records(sql=sql)
+            .read_text(encoding="utf-8").format(date=ds)
+    return pg_hook.get_records(sql=sql)
 
-    return json.dumps(records)
+# Generate DROP statment for a given partition
+@task
+def generate_sql_delete(policy):
+    return Path('include/data_retention_delete.sql') \
+        .read_text(encoding="utf-8").format(table=policy[2],
+                                            column=policy[3],
+                                            value=policy[4],
+                                           )
 
+# Generate DROP statment for a given partition
+@task
+def generate_sql_snapshot(policy):
+    return Path('include/data_retention_snapshot.sql').read_text(encoding="utf-8") \
+            .format(repository_name=policy[5],
+                    table_schema=policy[0],
+                    table_name=policy[1],
+                    table_fqn=policy[2],
+                    partition_column=policy[3],
+                    partition_value=policy[4],
+                   )
 
-def map_policy(policy):
-    return {
-        "table_schema": policy[0],
-        "table_name": policy[1],
-        "table_fqn": policy[2],
-        "column_name": policy[3],
-        "partition_value": policy[4],
-        "target_repository_name": policy[5],
-    }
-
-
-def snapshot_partitions(ti):
-    retention_policies = ti.xcom_pull(task_ids="retrieve_retention_policies")
-    policies_obj = json.loads(retention_policies)
-
-    for policy in policies_obj:
-        partition = map_policy(policy)
-
-        logging.info("Snapshoting partition %s = %s for table %s",
-                     partition["column_name"],
-                     partition["partition_value"],
-                     partition["table_fqn"],
-        )
-
-        PostgresOperator(
-            task_id=f"snapshot_{partition['table_fqn']}" \
-                    f"_{partition['column_name']}_{partition['partition_value']}",
-            postgres_conn_id="cratedb_connection",
-            sql=Path('include/data_retention_snapshot.sql').read_text(encoding="utf-8").format(
-                repository_name=partition["target_repository_name"],
-                table_schema=partition["table_schema"],
-                table_name=partition["table_name"],
-                table_fqn=partition["table_fqn"],
-                partition_column=partition["column_name"],
-                partition_value=partition["partition_value"],
-            ),
-        ).execute({})
-
-        PostgresOperator(
-            task_id=f"delete_{partition['table_fqn']}" \
-                    f"_{partition['column_name']}_{partition['partition_value']}",
-            postgres_conn_id="cratedb_connection",
-            sql=Path('include/data_retention_delete.sql').read_text(encoding="utf-8").format(
-                table=partition["table_fqn"],
-                column=partition["column_name"],
-                value=partition["partition_value"],
-            ),
-        ).execute({})
-
-
-with DAG(
-    dag_id="data-retention-snapshot-dag",
-    start_date=pendulum.datetime(2022, 1, 31, tz="UTC"),
+@dag(
+    start_date=pendulum.datetime(2021, 11, 19, tz="UTC"),
     schedule_interval="@daily",
     catchup=False,
-) as dag:
-    get_policies = PythonOperator(
-        task_id="retrieve_retention_policies",
-        python_callable=get_policies,
-        op_kwargs={
-            "logical_date": "{{ ds }}",
-        },
-    )
+)
+def data_retention_snapshot():
+    policies = get_policies()
+    sql_statements_snapshot = generate_sql_snapshot.expand(policy=policies)
+    sql_statements_delete = generate_sql_delete.expand(policy=policies)
 
-    apply_policies = PythonOperator(
-        task_id="apply_data_retention_policies",
-        python_callable=snapshot_partitions,
-        op_kwargs={},
-    )
+    reallocate = PostgresOperator.partial(
+        task_id="snapshot_partitions",
+        postgres_conn_id="cratedb_connection",
+    ).expand(sql=sql_statements_snapshot)
 
-    get_policies >> apply_policies
+    delete = PostgresOperator.partial(
+        task_id="delete_partitions",
+        postgres_conn_id="cratedb_connection",
+    ).expand(sql=sql_statements_delete)
+
+    reallocate >> delete
+
+data_retention_snapshot_dag = data_retention_snapshot()
