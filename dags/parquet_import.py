6559eb3b7fcd5b55db6ee9e9ba247dd5382bdf2f
karynzv <karyn.saz@gmail.com>
2022-09-02 18:37:42 -0300

    Changed DAG logic
    Changed the DAG to process many parquet files instead of one, based on the files available in the S3 Bucket.

--- dags/parquet_import.py
+++ dags/parquet_import.py
@@ -1,61 +1,97 @@
-import os
+import numpy as np
+from airflow.decorators import task, dag
 from datetime import datetime
+import logging
+from pathlib import Path
 from airflow.models import DAG
-from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.bash import BashOperator
+from airflow.providers.amazon.aws.operators.s3 import S3ListOperator
+from airflow.models import Variable
 
-def get_latest_month_year():
-    currentMonth = datetime.now().month - 1
-    currentYear = datetime.now().year
-    if currentMonth == 12:
-        currentYear = currentYear - 1
-    if currentMonth < 10:
-        currentMonth = f'0{currentMonth}'
-    return f'_{currentYear}-{currentMonth}'
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
+@dag(dag_id="nyc-taxi1",schedule_interval='@monthly',start_date=datetime(2022, 1, 1), catchup=False)
+def taskflow():
+    task_list_s3_files = S3ListOperator(
+        task_id="task_s3_list_files",
+        bucket='nyc-tlc',
+        prefix='trip data/yellow_tripdata',
+        delimiter='/',
+        aws_conn_id='s3_conn'
+    )  
+    @task
+    def get_processed_files():
+        pg_hook = PostgresHook(postgres_conn_id="cratedb_demo_connection")
+        records = pg_hook.get_records(sql='SELECT file_name FROM nyc_taxi.load_files_processed')
+        # flatten nested list as there is only one column
+        return list(map(lambda record: record[0], records))
 
-def download_from_s3_csv(ti,prefix_file:str,bucket_name: str) -> str:
-    latest_month_year = ti.xcom_pull(task_ids=['get_latest_month_year'])[0]
-    file_name_path = f'{local_path}{prefix_file}{latest_month_year}.csv'
-    s3_path = f's3://{bucket_name}/{prefix_file}{latest_month_year}.parquet'
-    os.system(f'parquet-tools csv "{s3_path}" > "{file_name_path}"')
-    return file_name_path
+    @task(task_id='task_list_unprocessed_files')
+    def list_unprocessed_files(data_urls_processed,ti=None):
+        data_urls_available = ti.xcom_pull(task_ids=['task_s3_list_files'])[0]
+        filename_retriever = lambda s: s.split('/')[-1]
+        transform_filename = np.vectorize(filename_retriever)
+        data_urls_available_transformed = transform_filename(data_urls_available)
+        return list(set(data_urls_available_transformed) - set(data_urls_processed))
 
-def process_new_file(ti):
-    file_path = ti.xcom_pull(task_ids=['download_from_s3_csv'])[0]
-    PostgresOperator(
-        task_id="copy_csv",
-        postgres_conn_id="cratedb_demo_connection",
-        sql=f"""
-            COPY nyc_taxi.load_trips_staging
-            FROM '{file_path}'
-            WITH (format = 'csv', empty_string_as_null = true)
-            RETURN SUMMARY;
-            """
-    )
+    task_list_unprocessed_files = list_unprocessed_files(get_processed_files())
+    (task_list_unprocessed_files << task_list_s3_files)
 
+    @task(task_id='task_download_files_s3')
+    def download_from_s3_csv(ti=None):
+        files = ti.xcom_pull(task_ids=['task_list_unprocessed_files'])[0]
+        filename_retriever = lambda s: s.split('/')[-1]
+        transform_filename = np.vectorize(filename_retriever)
+        files_names = transform_filename(files)
+        local_path = Variable.get("local_path")
+        s3_path = Variable.get("s3_path")
+        for file_name in files_names:
+            file_name_csv = file_name.replace('parquet','csv')
+            task_name = file_name.replace('parquet','')
+            print(f'parquet-tools csv "{s3_path}{file_name}" > "{local_path}{file_name_csv}"')
+            BashOperator(
+            task_id=f'download_from_s3_csv_{task_name}',
+            bash_command=f'parquet-tools csv "{s3_path}{file_name}" > "{local_path}{file_name_csv}"',
+            ).execute({})
 
-with DAG(
-    dag_id='parquet_to_csv_nyc_tlc',
-    schedule_interval='@monthly',
-    start_date=datetime(2022,3,1),
-    catchup=False
-) as dag:
-    task_get_latest_month_year = PythonOperator(
-        task_id='get_latest_month_year',
-        python_callable=get_latest_month_year)
+    @task(task_id='task_process_new_files')
+    def process_new_files(ti=None):
+        missing_files = ti.xcom_pull(task_ids="task_list_unprocessed_files")
+        s3_path = Variable.get("s3_path")
+        local_path = Variable.get("local_path")
+        for missing_file in missing_files:
+            logging.info(missing_file)
 
-    task_download_from_s3_csv = PythonOperator(
-        task_id='download_from_s3_csv',
-        python_callable=download_from_s3_csv,
-        op_kwargs={
-        'bucket_name':'nyc-tlc',
-        'prefix_file':'trip data/yellow_tripdata'
-        })
+            file_name = missing_file.replace('parquet','csv')
 
-    task_process_new_file = PythonOperator(
-        task_id='process_new_file',
-        python_callable=process_new_file)
+            PostgresOperator(
+                task_id=f"copy_{file_name}",
+                postgres_conn_id="cratedb_demo_connection",
+                sql=f"""
+                        COPY nyc_taxi.load_trips_staging_parquet
+                        FROM '{local_path}{file_name}'
+                        WITH (format = 'csv', empty_string_as_null = true)
+                        RETURN SUMMARY;
+                    """
+            ).execute({})
 
-    task_get_latest_month_year >> task_download_from_s3_csv
-    task_download_from_s3_csv >> task_process_new_file
+            PostgresOperator(
+                task_id=f"log_{file_name}",
+                postgres_conn_id="cratedb_demo_connection",
+                sql=Path('include/taxi-insert.sql').read_text(encoding="utf-8"),
+            ).execute({})
+
+            PostgresOperator(
+                task_id=f"mark_processed_{file_name}",
+                postgres_conn_id="cratedb_demo_connection",
+                sql=f"INSERT INTO nyc_taxi.load_files_processed_parquet VALUES ('{s3_path}{missing_file}');",
+            ).execute({})
+
+            PostgresOperator(
+                task_id=f"purge_staging_{file_name}",
+                postgres_conn_id="cratedb_demo_connection",
+                sql="DELETE FROM nyc_taxi.load_trips_staging_parquet;"
+            ).execute({})
+    task_list_unprocessed_files >> download_from_s3_csv() >> process_new_files()
+dag = taskflow()
