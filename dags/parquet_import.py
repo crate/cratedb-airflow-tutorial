e7e230e42b7fc56ae0d1e4d6edc924aeb8564a53
karynzv <karyn.saz@gmail.com>
2022-09-13 18:12:25 -0300

    Renaming of variables
    Changed a couple of variable names to be more meaningful

--- dags/parquet_import.py
+++ dags/parquet_import.py
@@ -10,7 +10,7 @@ from airflow.models import Variable
 def taskflow():
     @task(task_id='get_month_year')
     def get_month_year(ds=None):
-        currentMonth = int(ds.split('-')[1])
+        currentMonth = int(ds.split('-')[1]) - 2
         currentYear = int(ds.split('-')[0])
         if currentMonth == 12:
             currentYear = currentYear - 1
@@ -20,12 +20,12 @@ def taskflow():
         return f'_{currentYear}-{currentMonth}'
 
 
-    @task(task_id='download_files_s3')
+    @task(task_id='download_file_s3')
     def download_from_s3_csv(file_date):
-        local_path = Variable.get("local_path")
+        destination_path = Variable.get("destination_path")
         s3_path = Variable.get("s3_path")
         file_name = f'yellow_tripdata{file_date}'
-        file_path = f'{local_path}{file_name}.csv'
+        file_path = f'{destination_path}{file_name}.csv'
         BashOperator(
             task_id='download_from_s3_csv',
             bash_command=f'parquet-tools csv "{s3_path}{file_name}.parquet" > "{file_path}"',
@@ -34,14 +34,12 @@ def taskflow():
    
     @task(task_id='process_new_files')
     def process_new_files(new_csv_file):
-        local_path = Variable.get("local_path")
-
         PostgresOperator(
                 task_id="copy_new_csv_file",
                 postgres_conn_id="cratedb_demo_connection",
                 sql=f"""
                         COPY nyc_taxi.load_trips_staging
-                        FROM '{local_path}{new_csv_file}'
+                        FROM '{new_csv_file}'
                         WITH (format = 'csv', empty_string_as_null = true)
                         RETURN SUMMARY;
                     """
