3a59e851e3b8b2b813e91de81492e9dac6c63b87
karynzv <karyn.saz@gmail.com>
2022-09-13 21:09:36 -0300

    Pylint fixes
    Checked pylint errors and fixed

--- dags/nyc_taxi_dag.py
+++ dags/nyc_taxi_dag.py
@@ -1,12 +1,15 @@
-from airflow.decorators import task, dag
-from datetime import datetime
 from pathlib import Path
+from datetime import datetime
+from airflow.decorators import task, dag
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.bash import BashOperator
 from airflow.models import Variable
 
 
-@dag(dag_id="nyc-taxi-parquet",schedule_interval='@monthly',start_date=datetime(2009, 1, 1), catchup=True)
+@dag(dag_id="nyc-taxi-parquet",
+schedule_interval='@monthly',
+start_date=datetime(2009, 1, 1),
+catchup=True)
 def taskflow():
     @task(task_id='get_month_year')
     def get_month_year(ds=None):
@@ -31,7 +34,7 @@ def taskflow():
             bash_command=f'parquet-tools csv "{s3_path}{file_name}.parquet" > "{file_path}"',
             ).execute({})
         return file_path
-   
+
     @task(task_id='process_new_files')
     def process_new_files(new_csv_file):
         PostgresOperator(
