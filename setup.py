e9afaa114b75c1a840b1ffcf15d1678648b929bb
Niklas Schmidtmer <niklas@crate.io>
2022-11-07 09:13:00 +0100

    Align apache-airflow version in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="crate-airflow-tutorial",
     packages=find_packages(),
     python_requires=">=3.9",
-    install_requires=["apache-airflow==2.4.1"],
+    install_requires=["apache-airflow==2.4.2"],
     extras_require={
         "develop": ["pylint==2.15.3", "black==22.8.0"],
         "testing": ["pytest==7.1.3"],
