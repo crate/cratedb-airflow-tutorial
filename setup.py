0d396c22c0b4181517205f917274d346741a7a2b
Niklas Schmidtmer <niklas@crate.io>
2023-09-07 07:54:59 +0200

    Update Python to 3.11 to align with base image update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="crate-airflow-tutorial",
     packages=find_packages(),
-    python_requires=">=3.9",
+    python_requires=">=3.11",
     install_requires=["apache-airflow==2.7.0"],
     extras_require={
         "develop": [
