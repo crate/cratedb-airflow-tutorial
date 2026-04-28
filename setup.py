5576eb1cd50bdccf936b73094611a055c82f248d
Niklas Schmidtmer <niklas@crate.io>
2026-04-28 12:14:49 +0200

    Update versions

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name="crate-airflow-tutorial",
     packages=find_packages(),
-    python_requires=">=3.12",
+    python_requires=">=3.13",
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     install_requires=["apache-airflow==3.2.0"],
     extras_require={
         "develop": [
-            "ruff==0.15.8",
+            "ruff==0.15.12",
         ],
         "testing": [
-            "pytest==9.0.2",
+            "pytest==9.0.3",
         ],
     },
 )
